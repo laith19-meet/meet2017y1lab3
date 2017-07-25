6dd8ae1b35f58a4c0d3af7105b7154a1ba2b7746
laith19-meet <laith19@meet.mit.edu>
2017-07-25 12:22:40 +0300

    Stopping here

---
+++ how to say hello in different languages.py
@@ -0,0 +1,22 @@
+print("hello world is how you say hello world in english.")
+
+print("Γειά σου Κόσμε is how you say hello world in greek.")
+
+print("Olá Mundo is how you say hello world in portugese.")
+
+print("Ahoj světe is how you say hello world in czech.")
+
+print("안녕 세상 is how you say hello world in korean .")
+
+print("你好，世界 is how you say hello world in chinese.")
+
+print("नमस्ते दुनिया is how you say hello world in hindi.")
+
+print("Salamu, Dunia is how you say hello world in swahili.")
+
+print("Bonjour le monde  is how you say hello world in french.")
+
+print("שלום עולם is how you say hello world in hebrew.")
+
+print("مرحبا بالعالم is how you say hello world in arabic.")
+
---
+++ lab2 module3.py
@@ -0,0 +1,175 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello it is me 5")
+hello it is me 5
+>>> print ("this is day 1")
+this is day 1
+>>> 10 = x
+SyntaxError: can't assign to literal
+>>> x=10
+>>> ny_number= x
+>>> int=int(mu_number)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    int=int(mu_number)
+NameError: name 'mu_number' is not defined
+>>> int = int (my_number)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    int = int (my_number)
+NameError: name 'my_number' is not defined
+>>> 
+>>> int = int (ny_number)
+>>> int
+10
+>>> nu_number.replace ("x,go")
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    nu_number.replace ("x,go")
+NameError: name 'nu_number' is not defined
+>>> ny_number.replace ("x,go")
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    ny_number.replace ("x,go")
+AttributeError: 'int' object has no attribute 'replace'
+>>> "the" +3
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    "the" +3
+TypeError: Can't convert 'int' object to str implicitly
+>>> "don't" + "forget" + "to" + "consereve" + "water"
+"don'tforgettoconserevewater"
+>>> "the"*3 + "beutiful" + "world"
+'thethethebeutifulworld'
+>>> 2*"true"
+'truetrue'
+>>> a=save
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    a=save
+NameError: name 'save' is not defined
+>>> my_string = "b
+SyntaxError: EOL while scanning string literal
+>>> my_string = "abc"
+>>> my_string[0]
+'a'
+>>> a=save
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    a=save
+NameError: name 'save' is not defined
+>>> a='save'
+>>> b='the'
+>>> c='world'
+>>> print("a + b + c")
+a + b + c
+>>> print("a"+"b"+"c")
+abc
+>>> print('a'+'b'+'c')
+abc
+>>> a+b+c
+'savetheworld'
+>>> a + b + c
+'savetheworld'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> my_string=ab
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    my_string=ab
+NameError: name 'ab' is not defined
+>>> my_string="ab"
+>>> a='4'
+>>> b='panda bears'
+>>> a+b
+'4panda bears'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> abc="teaching a child"
+>>> abc
+'teaching a child'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> my_string=abc
+>>> abc="teaching a child"
+>>> abc
+'teaching a child'
+>>> my_string.len=len(my_string)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    my_string.len=len(my_string)
+AttributeError: 'str' object has no attribute 'len'
+>>> abc.len=len(abc)
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    abc.len=len(abc)
+AttributeError: 'str' object has no attribute 'len'
+>>> int(abc)
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    int(abc)
+TypeError: 'int' object is not callable
+>>> len(a)
+1
+>>> my_string='my name is laith'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> my_string='a'
+>>> a='my name is laith'
+>>> len(a)
+16
+>>> upper(a)
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    upper(a)
+NameError: name 'upper' is not defined
+>>> a.upper()
+'MY NAME IS LAITH'
+>>> a.lower()
+'my name is laith'
+>>> a.capitalized()
+Traceback (most recent call last):
+  File "<pyshell#70>", line 1, in <module>
+    a.capitalized()
+AttributeError: 'str' object has no attribute 'capitalized'
+>>> a.capitalize()
+'My name is laith'
+>>> a.swapcase()
+'MY NAME IS LAITH'
+>>> a.upper()
+'MY NAME IS LAITH'
+>>> a.replace("i","o")
+'my name os laoth'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> a= "MEET"
+>>> b= "Meet"
+>>> c= "meet"
+>>> 
+>>> a==b
+False
+>>> a==c
+False
+>>> b==c
+False
+>>> a !=b
+True
+>>> a !=c
+True
+>>> b !=c
+True
+>>> 
